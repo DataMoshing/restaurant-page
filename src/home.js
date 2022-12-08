362cd9897224ac909b3493d66bccf224e7517c7f
datamoshing <imkiloss@gmail.com>
2022-12-08 10:09:58 -0700

    Added logic for main home content

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-const displayData = () => {
+const mainHeader = () => {
     const content = document.getElementById("content");
     const header = document.createElement("header");
     const nav = document.createElement("nav");
@@ -6,7 +6,7 @@ const displayData = () => {
     const homeLi = document.createElement("li");
     const menuLi = document.createElement("li");
     const contactLi = document.createElement("li");
-    const mainHeading = document.createElement("h1");
+    const topHeading = document.createElement("h1");
 
     homeLi.textContent = "Home";
     homeLi.className = "home";
@@ -18,7 +18,8 @@ const displayData = () => {
     contactLi.className = "contact"
     headerUl.append(homeLi, menuLi, contactLi);
 
-    mainHeading.textContent = "Lon Lon Cafe";
+    topHeading.textContent = "Lon Lon Cafe";
+    topHeading.classList = "top-heading";
 
     const hamburgerImg = new Image();
     hamburgerImg.src = "/src/imgs/hamburger.jpg";
@@ -28,9 +29,68 @@ const displayData = () => {
     nav.append(headerUl);
     header.append(nav)
     content.append(header);
-    content.append(mainHeading);
+    nav.append(topHeading);
     content.append(hamburgerImg);
     document.body.append(header, content)
 }
 
-export { displayData }
+const mainContent = () => {
+    const content = document.getElementById("content");
+    const mainDiv = document.createElement("div");
+    const mainDescription = document.createElement("p");
+    const mainScheduleDiv = document.createElement("div")
+    const mainSchedule = document.createElement("h2");
+    const weekday_1 = document.createElement("p");
+    const weekday_2 = document.createElement("p");
+    const weekday_3 = document.createElement("p");
+    const weekday_4 = document.createElement("p");
+    const weekday_5 = document.createElement("p");
+    const weekend_1 = document.createElement("p");
+    const weekend_2 = document.createElement("p");
+
+    mainDescription.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+    mainDiv.className = "main-description";
+    mainScheduleDiv.className = "main-schedule-div";
+
+    mainSchedule.textContent = "Hours";
+    mainSchedule.className = "hrs-header";
+    weekday_1.textContent = "Monday: 7am - 8pm";
+    weekday_2.textContent = "Tuesday: 6am - 8pm";
+    weekday_3.textContent = "Wednesday: 6am - 8pm";
+    weekday_4.textContent = "Thursday: 9am - 8pm";
+    weekday_5.textContent = "Friday: 6am - 11pm";
+    weekend_1.textContent = "Saturday: Closed";
+    weekend_2.textContent = "Sunday: Closed";
+
+    const reviewDiv = document.createElement("div");
+    reviewDiv.className = "review-div";
+    const reviewHeader = document.createElement("h3");
+    reviewHeader.textContent = "Reviews";
+    reviewHeader.className = "review-header";
+
+    const reviewPlaceholder_1 = document.createElement("p");
+    const reviewPlaceholder_2 = document.createElement("p");
+    const reviewPlaceholder_3 = document.createElement("p");
+    const reviewPlaceholder_4 = document.createElement("p");
+
+    reviewPlaceholder_1.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+
+    reviewPlaceholder_2.textContent = "Auctor urna nunc idcursus metus aliquam eleifend mi in. Suscipit. Sit amet risus nullam eget felis."
+
+    reviewPlaceholder_3.textContent = "Quis auctor elit sed vulputate mi."
+
+    reviewPlaceholder_4.textContent = "Ut tristique et egestas quis ipsum suspendisse ultrices."
+
+    content.append(mainDiv);
+    mainDiv.append(mainDescription);
+    content.append(mainScheduleDiv);
+    mainScheduleDiv.append(mainSchedule);
+    mainScheduleDiv.append(weekday_1, weekday_2, weekday_3, weekday_4, weekday_5, weekend_1, weekend_2);
+    content.append(reviewHeader);
+    content.append(reviewDiv);
+    reviewDiv.append(reviewPlaceholder_1, reviewPlaceholder_2, reviewPlaceholder_3, reviewPlaceholder_4);
+    document.body.append(content)
+}
+
+export { mainHeader, mainContent }
