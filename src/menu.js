31105f8bace178d1e36e6af7061b538d3d0eb98b
datamoshing <imkiloss@gmail.com>
2022-12-12 15:44:43 -0700

    Created class list for drink prices

--- src/menu.js
+++ src/menu.js
@@ -3,8 +3,11 @@ import { content } from "./home.js";
 const menuContent = () => {
     const menuDiv = document.createElement("div");
     const coffeeHeader = document.createElement("h2");
+    const coffeeHeaderDiv = document.createElement("div");
     const notCoffeeHeader = document.createElement("h2");
+    const notCoffeeHeaderDiv = document.createElement("div");
     const foodHeader = document.createElement("h2");
+    const foodDiv = document.createElement("div");
     const drink_1 = document.createElement("p");
     const drink_1_Price = document.createElement("p");
     const drink_2 = document.createElement("p");
@@ -27,37 +30,53 @@ const menuContent = () => {
 
     coffeeHeader.textContent = "COFFEE  +  ESPRESSO";
     coffeeHeader.classList = "coffee-header";
+    coffeeHeaderDiv.classList = "coffee-header-div";
     notCoffeeHeader.textContent = "NOT COFFEE";
     notCoffeeHeader.classList = "not-coffee-header";
+    notCoffeeHeaderDiv.classList = "not-coffee-div";
     foodHeader.textContent = "FOOD";
     foodHeader.classList = "food-header";
+    foodDiv.classList = "food-div";
+
 
     drink_1.textContent = "Cold brew";
     drink_1_Price.textContent = "5.5";
+    drink_1_Price.classList = "drink-price";
     drink_2.textContent = "Latte";
     drink_2_Price.textContent = "4.5";
+    drink_2_Price.classList = "drink-price"
     drink_3.textContent = "Espresso";
     drink_3_Price.textContent = "3.25";
+    drink_3_Price.classList = "drink-price";
     drink_4.textContent = "Hot Chocolate";
     drink_4_Price.textContent = "3.5"
+    drink_4_Price.classList = "drink-price";
     drink_5.textContent = "Iced Tea";
     drink_5_Price.textContent = "4";
+    drink_5_Price.classList = "drink-price";
     drink_6.textContent = "Matcha Latte";
     drink_6_Price.textContent = "5.5";
+    drink_6_Price.classList = "drink-price";
     foodItem_1.textContent = "Avocado Toast";
     foodItem_1_Price.textContent = "10";
+    foodItem_1_Price.classList = "food-price";
     foodItem_2.textContent = "Smoked Salmon Bagel";
     foodItem_2_Price.textContent = "15";
+    foodItem_2_Price.classList = "food-price"
     foodItem_3.textContent = "Granola Bowl";
     foodItem_3_Price.textContent = "9";
+    foodItem_3_Price.classList = "food-price";
 
     content.append(menuDiv);
-    menuDiv.append(coffeeHeader);
-    menuDiv.append(drink_1, drink_1_Price, drink_2, drink_2_Price, drink_3, drink_3_Price);
-    menuDiv.append(notCoffeeHeader);
-    menuDiv.append(drink_4, drink_4_Price, drink_5, drink_5_Price, drink_6, drink_6_Price);
+    menuDiv.append(coffeeHeader)
+    menuDiv.append(coffeeHeaderDiv)
+    coffeeHeaderDiv.append(drink_1, drink_1_Price, drink_2, drink_2_Price, drink_3, drink_3_Price);
+    menuDiv.append(notCoffeeHeader)
+    menuDiv.append(notCoffeeHeaderDiv);
+    notCoffeeHeaderDiv.append(drink_4, drink_4_Price, drink_5, drink_5_Price, drink_6, drink_6_Price);
     menuDiv.append(foodHeader);
-    menuDiv.append(foodItem_1, foodItem_1_Price, foodItem_2, foodItem_2_Price, foodItem_3, foodItem_3_Price);
+    menuDiv.append(foodDiv);
+    foodDiv.append(foodItem_1, foodItem_1_Price, foodItem_2, foodItem_2_Price, foodItem_3, foodItem_3_Price);
     document.body.append(content);
 }
 
