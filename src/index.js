04d2d9c93e081a091b2106ca2b414abd86f766d5
datamoshing <imkiloss@gmail.com>
2022-12-11 21:26:30 -0700

    Created tab-switching logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import { mainHeader, mainContent } from "./home.js";
+import { mainHeader, mainContent, menuLi, homeLi, contactLi, content, } from "./home.js";
+import { menuContent } from "./menu.js";
 import "./style.css";
+
+
+homeLi.addEventListener("click", () => {
+    content.textContent = ""
+    mainContent()
+})
+
+menuLi.addEventListener("click", () => {
+    content.textContent = ""
+    menuContent()
+})
 
 mainHeader()
 mainContent()
