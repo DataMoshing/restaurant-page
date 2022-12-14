cea7cf5021e8cfb118223a8ed612f28c98f06955
datamoshing <imkiloss@gmail.com>
2022-12-14 12:46:09 -0700

    Fixed image file paths

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import { mainHeader, mainContent, menuLi, homeLi, contactLi, content, } from "./
 import { menuContent } from "./menu.js";
 import { contactContent } from "./contact.js";
 import "./style.css";
-import "../src/imgs/hamburger.jpg";
-import "../src/imgs/bubble-tea-svgrepo-com.svg";
-import "../src/imgs/email-svgrepo-com.svg";
-import "../src/imgs/location-svgrepo-com.svg";
-import "../src/imgs/phone-svgrepo-com.svg"
-
 
 homeLi.addEventListener("click", () => {
     content.textContent = ""
